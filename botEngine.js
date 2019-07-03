476515011f98cd1bcd405ac9372c9ba89199dce1
itsTonyDavid <antoniod_98@hotmail.com>
2019-07-02 21:23:40 -0500

    ptm espero esto funcione ya

--- botEngine.js
+++ botEngine.js
@@ -1,26 +1,21 @@
 const Twit = require('twit');
-const twitterCredentials = getCredentials();
 
-function getCredentials(){
-  if ( process.env.NODE_ENV == 'production') {
-    const twitterCredentials2 = {
-      consumer_key: process.env.twitter_consumer_key,
-      consumer_secret: process.env.twitter_consumer_secret,
-      access_token: process.env.twitter_access_token,
-      access_token_secret: process.env.twitter_access_token_secret
+if ( process.env.NODE_ENV == 'production') {
+  const twitterCredentials = {
+    consumer_key: process.env.twitter_consumer_key,
+    consumer_secret: process.env.twitter_consumer_secret,
+    access_token: process.env.twitter_access_token,
+    access_token_secret: process.env.twitter_access_token_secret
+  }
+}
+else {
+    const credentials = require('./credentials')
+    const twitterCredentials = {
+      consumer_key: credentials.twitter_consumer_key,
+      consumer_secret: credentials.twitter_consumer_secret,
+      access_token: credentials.twitter_access_token,
+      access_token_secret: credentials.twitter_access_token_secret
     }
-    return twitterCredentials2;
-  }
-  else {
-      const credentials = require('./credentials')
-      const twitterCredentials2 = {
-        consumer_key: credentials.twitter_consumer_key,
-        consumer_secret: credentials.twitter_consumer_secret,
-        access_token: credentials.twitter_access_token,
-        access_token_secret: credentials.twitter_access_token_secret
-      }
-      return twitterCredentials2;
-  }
 }
 
 var T = new Twit(twitterCredentials);
